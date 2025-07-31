f7e4500257c9004b63270376e4e976b3622c57a3
Rafael Cabanillas <rafaelcabanillas12@gmail.com>
2025-07-31 15:31:05 -0600

    Fix: 'sameSite' was strict, changed to none

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -35,9 +35,9 @@ export const login = async (req: Request, res: Response) => {
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'none', // <-- this allows cross-origin
             path: '/api/auth/refresh',
-            maxAge: 7 * 24 * 60 * 60 * 1000,
+            maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
         // ✅ Send access token + user
