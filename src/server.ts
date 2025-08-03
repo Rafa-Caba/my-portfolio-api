9d075101f8f7107196a7ff398d0d557aa909e6ba
Rafael Cabanillas <rafaelcabanillas12@gmail.com>
2025-08-03 12:32:34 -0600

    Frontend url update

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ const app: Application = express();
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://my-portfolio-six-tan-41.vercel.app'
+        'https://my-portfolio-cabanillas.vercel.app'
     ],
     credentials: true
 }));
